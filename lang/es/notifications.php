224c15966e5303a8466f4320864805b7f4f1cf9c
andresayac <andresayac@gmail.com>
2025-04-28 22:39:08 -0500

    Add Spanish language support for various application components

---
+++ lang/es/notifications.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'stocks' => [
+        'single-runs-out'     => 'El stock de :product se agotará',
+        'single-out-of-stock' => 'El stock de :product está agotado',
+        'multiple-runs-out'   => 'Hay :count productos que están a punto de agotarse',
+        'field_stock'         => 'El stock restante es :stock',
+        'title'               => 'Tu stock está a punto de agotarse',
+    ],
+];
