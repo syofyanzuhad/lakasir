78acae011e3053687b5765d98cab9092fb963497
muhamiyan <apriliansyahidris@gmail.com>
2025-05-07 14:18:40 +0700

    fix: update policy for supplier, selling and payment method

--- app/Filament/Tenant/Resources/PaymentMethodResource.php
+++ app/Filament/Tenant/Resources/PaymentMethodResource.php
@@ -2,11 +2,9 @@
 
 namespace App\Filament\Tenant\Resources;
 
-use App\Features\PaymentMethod as FeaturesPaymentMethod;
 use App\Filament\Tenant\Resources\PaymentMethodResource\Pages;
 use App\Models\Tenants\PaymentMethod;
 use App\Traits\HasTranslatableResource;
-use Filament\Facades\Filament;
 use Filament\Forms\Components\Card;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\TextInput;
@@ -15,7 +13,6 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Laravel\Pennant\Feature;
 
 class PaymentMethodResource extends Resource
 {
@@ -114,10 +111,5 @@ class PaymentMethodResource extends Resource
             'create' => Pages\CreatePaymentMethod::route('/create'),
             'edit' => Pages\EditPaymentMethod::route('/{record}/edit'),
         ];
-    }
-
-    public static function canViewAny(): bool
-    {
-        return ! (! Feature::active(FeaturesPaymentMethod::class)) ?? Filament::auth()->user()->can('read payment method');
     }
 }
