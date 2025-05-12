db2fc93bfa5028501a2408fd835beab6f9cedae3
Jonathon Klem <jonathonklem@gmail.com>
2025-05-12 12:46:46 -0500

    Make modifications to allow migration/seeding in sqlite environment

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,7 +15,9 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        if (DB::getDriverName() !== 'sqlite') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        }
         Category::truncate();
         Category::create([
             'name' => "UMUM"
