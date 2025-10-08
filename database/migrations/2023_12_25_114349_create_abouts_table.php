a02d39a183f58ae25319dca1a6fef801ffc4f9a2
Syofyan Zuhad <mail@syofyanzuhad.dev>
2025-10-08 22:23:42 +0700

    Drop existing 'abouts' table before creating a new one in migration for improved schema management.

--- database/migrations/2023_12_25_114349_create_abouts_table.php
+++ database/migrations/2023_12_25_114349_create_abouts_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('abouts');
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(TenantUser::class);
